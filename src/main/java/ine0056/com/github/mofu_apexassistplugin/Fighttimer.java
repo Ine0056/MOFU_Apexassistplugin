48d4f09379154f2dd10a3d3ed18c934f3f3d3fb5
いね <ine0056@gmail.com>
2021-07-27 23:43:43 +0900

    Update Fighttimer.java

--- src/main/java/ine0056/com/github/mofu_apexassistplugin/Fighttimer.java
+++ src/main/java/ine0056/com/github/mofu_apexassistplugin/Fighttimer.java
@@ -1,12 +1,18 @@
 package ine0056.com.github.mofu_apexassistplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 import ine0056.com.github.mofu_apexassistplugin.MOFU_Apexassistplugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Team;
 
 public class Fighttimer extends JavaPlugin{
+
+    private static int mopex20slefttask;
+    private String mopexteam;
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -14,10 +20,14 @@ public class Fighttimer extends JavaPlugin{
             if (command.getName().equalsIgnoreCase("mopex")) {
                 if (args.length != 0) {
                     if (args[0].equalsIgnoreCase("gamestart")) {
+                            Bukkit.broadcastMessage(ChatColor.RED + "30" + ChatColor.AQUA +"秒後にゲームを開始します...");
+                            mopex20slefttask = Bukkit.getScheduler().runTaskLater(this, () -> {
 
+                            }, 20*20L).getTaskId();
                     }
                 }
             }
         }
+        return false;
     }
 }
